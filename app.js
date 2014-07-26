1f5820af2d60535c26498ea188e149e1042632a0
Alex <aletormat@gmail.com>
2014-07-26 13:42:11 +0200

    Retweets
    Retweets from twitter

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 /*
  * Load external modules and init variables
  */
-var twitter = require('ntwitter'),
+var twitter = require('twit'),
     http = require('http'),
-    tweeted = {},
+    tweeted = [],
+    idstweeteds = [],
     load_time = Math.round(new Date().getTime() / 10000),
-    score_threshold = 100;
+    score_threshold = 40;
  
+
 /*
  * Setup twitter api
  */
@@ -18,13 +20,67 @@ var twit = new twitter({
 });
  
  
- 
 /*
  * Primary wrapper function
  */
 function gogo() {
  
     console.log('Making request');
+    
+    //SEARCH FOR RT on twitter
+    //date
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd;
+    } 
+
+    if(mm<10) {
+        mm='0'+mm;
+    } 
+
+    today = yyyy+'-'+mm+'-'+dd;
+
+    twit.get('search/tweets', {q: 'pixelart since:'+today, count: 30}, function(err, data, response){
+       // console.log(data.statuses[0].id);
+        rt = data.statuses;
+        var len = rt.length;
+       // console.log(len);
+        var start = 'RT';
+
+
+        for(var i=0; i<len; i++){
+            var rw = rt[i],
+                retweets = rw.retweet_count,
+                favs = rw.favorite_count,
+                text = rw.text.toString(),
+                id = rw.id_str,
+                user = rw.screen_name;
+
+            if(!(text.indexOf(start) === 0) && (favs>2 || retweets>1) && user!='pixelartbot' && !idstweeteds[id]){
+                console.log("Retweets: "+retweets+'\n'+
+                            "Favs: "+favs+'\n'+
+                            "text: "+text+'\n'+
+                            "id: "+id+'\n');
+
+                twit.post('statuses/retweet/:id', { id: id }, function (err, data, response) {
+                    if(err){
+                        console.log(err);
+                    }
+                    idstweeteds.push(id);
+                });
+
+            }
+        }
+    });
+    
+
+
+
+    //SEARCH ON REDDIT
     var options = {
         host : 'www.reddit.com',
         port : 80,
@@ -36,7 +92,7 @@ function gogo() {
      * Send an http request to the reddit API
      */
     http.get(options, function(res) {
-        console.log("Got response: " + res.statusCode);
+       // console.log("Got response: " + res.statusCode);
  
         /*
          * Check we have a status 200 response.  If not then do nothing.  Under heavy
@@ -54,8 +110,7 @@ function gogo() {
                 data = data.join('');
  
                 var reddit = JSON.parse(data).data.children,
-                    len = reddit.length,
-                    j;
+                    len = reddit.length;
  
                 // Limit top 15
                 if(len > 15) {
@@ -64,24 +119,44 @@ function gogo() {
  
  
                 // Loop through results and parse
-                for(j=0;j<len;j++) {
+                for(var j=0;j<len;j++) {
+
                     var ar = reddit[j].data,
                         score = ar.score,
                         created = ar.created,
                         title = ar.title,
                         url = ar.url,
-                        domain = ar.domain;
+                        domain = ar.domain,
+                        id = ar.id;
                     
 
-                    if(!tweeted[title] && (domain == "imgur.com" || domain=="youtube.com" || domain=="media.indiedb.com" || domain=="pixeljoint.com" || domain=="i.imgur.com") && created>load_time){
+                    if(!tweeted[id] && (domain == "imgur.com" || domain=="youtube.com" || domain=="media.indiedb.com" || domain=="pixeljoint.com" || domain=="i.imgur.com") && created>load_time){
                     // Send tweet
                     title = shortenTweet(title);
-                    console.log([title, ' Url: ', url, ' '].join(''));
-                    twit.updateStatus([title, ' ', url, ' #pixelArt'].join(''), function(err) {
-                    if(err) console.log('Twit err: ' + err);
-                    });
+                    
+                    //Get the image from imgur
+                    if(domain == "imgur.com"){
+                        url = url.replace(/^https?:\/\//,'');
+                        url = url.split('/');
+                        newUrl = url[0]+'/gallery/'+url[1];
+                        url = newUrl;
+                        console.log('NEW URL '+url);
 
-                    tweeted[title] = true;
+                    }
+
+                    console.log([title, ' Url: ', url, ' '].join(''));
+                    //twit
+                   /*twit.post('statuses/update', {status: [title, ' ', url, ' #pixelArt'].join('')}, function(err, data, response){
+                        console.log(data);
+                    });*/
+
+
+                    //ntwitter
+                    //twit.updateStatus([title, ' ', url, ' #pixelArt'].join(''), function(err) {
+                    //if(err) console.log('Twit err: ' + err);
+                    //});
+
+                    tweeted.push(id);
                 }(ar);
                     
                 }
@@ -95,7 +170,6 @@ function gogo() {
     });
  
 }
- 
  
 var shortenTweet = function(tweet) {
     if(tweet.length >= 80) {
