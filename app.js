186ac0fcda5ade328d668e03c6892b6494759f6b
Alex <aletormat@gmail.com>
2014-07-26 13:57:10 +0200

    cc

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ var twitter = require('twit'),
 var twit = new twitter({
   consumer_key: '****',
   consumer_secret: '****',
-  access_token_key: '****',
+  access_token: '****',
   access_token_secret: '****'
 });
+ 
  
  
 /*
